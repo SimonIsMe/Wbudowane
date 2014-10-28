a3ae980379560e59feba2f0b5028f20b49c0fb11
Szymon Skrzyński <skrzynski.szymon@gmail.com>
2014-10-28 14:12:19 +0100

    Natężenie - usunięcie print'ów

--- 2/natezenie/natezenie.ino
+++ 2/natezenie/natezenie.ino
@@ -3,58 +3,37 @@ int photocellPin = 0;
 int ledPin = 11;
 int buttonLeftPin = 4;
 int buttonRightPin = 2;
-int internalLedPin = 13;
 
 int minLed = 0, maxLed = 255;
 int minCell = 0, maxCell = 1023;
 
 int photocellVal, ledVal, btnLVal, btnRVal;        
 
-int counter = 0;
-
 void setup(void) 
 {
   pinMode(buttonLeftPin, INPUT);
   pinMode(buttonRightPin, INPUT);
-  pinMode(internalLedPin, OUTPUT);
-  Serial.begin(9600);
 }
  
 void loop(void) 
 {
-
   photocellVal = analogRead(photocellPin);
   btnLVal = digitalRead(buttonLeftPin);
   btnRVal = digitalRead(buttonRightPin);
-  
-  counter++;
-  if (counter % 100 == 0) {
-    Serial.print(photocellVal);
-    Serial.print(" => ");
-    Serial.println(ledVal);
-    counter = 0;
-  } 
-  
+ 
   if (btnLVal == HIGH) {
-    Serial.println("L");
     minCell = photocellVal;
-  //  maxCell = 1023;
+    //maxCell = 1023;
   }
-  if (btnRVal == HIGH) {
-    Serial.println("R");    
+  if (btnRVal == HIGH) { 
     maxCell = photocellVal;
-   // minCell = 0;
+    //minCell = 0;
   }
   
   photocellVal = constrain(photocellVal, minCell, maxCell);
   photocellVal = maxCell - photocellVal + minCell;
   ledVal = map(photocellVal, minCell, maxCell, 0, 255);
-  if (ledVal < 0) {
-    ledVal = 0;
-  }
-  if (ledVal > 255) {
-    ledVal = 255;
-  }
+  ledVal = constrain(ledVal, 0, 255);
   
   analogWrite(ledPin, ledVal);
   
