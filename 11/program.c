98b90bec6fd9f02d631c340ea2b4395b54b424ee
Szymon Skrzyński <skrzynski.szymon@gmail.com>
2014-12-01 21:53:41 +0100

    11 gotowe, 14 bez liczenia czasu

--- 11/program.c
+++ 11/program.c
@@ -1,83 +1,81 @@
-/* C99 */
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
 #include <math.h>
+#include <time.h>
 
-unsigned int pierwszySposob(unsigned int wartosc) 
+unsigned int pierwszySposob(unsigned int wartosc)
 {
-	
-	//	4294901760 = 1111 1111 1111 1111 0000 0000 0000 0000	<=	2 części po 16 bitów
-	//	65535 = 0000 0000 0000 0000 1111 1111 1111 1111 	<=	2 części po 16 bitów
-	
-	//	4278255360 = 1111 1111 0000 0000 1111 1111 0000 0000	<=	4 części po 8 bity
-	//	16711935 = 0000 0000 1111 1111 0000 0000 1111 1111	<=	4 części po 8 bity
-	
-	//	4042322160 = 1111 0000 1111 0000 1111 0000 1111 0000	<=	8 części po 4 bity
-	//	252645135 = 0000 1111 0000 1111 0000 1111 0000	1111	<=	8 części po 4 bity
-	
-	//	3435973836 = 1100 1100 1100 1100 1100 1100 1100 1100	<=	16 części po 2 bity
-	//	858993459 = 0011 0011 0011 0011 0011 0011 0011 0011	<=	16 części po 2 bity
-	
-	//	2863311530 = 1010 1010 1010 1010 1010 1010 1010 1010	<=	32 części po 1 bicie
-	//	1431655765 = 0101 0101 0101 0101 0101 0101 0101 0101	<=	32 części po 1 bicie
-	
-    
-    //	Dzielę 32 bitową liczbę na dwie 16 bitowe części, które zamieniam 
+    //	Dzielę 32 bitową liczbę na dwie 16 bitowe części, które zamieniam
     //	ze sobą miejscami
     //	itd...
-    wartosc = (wartosc & 4294901760) >> 16 | (wartosc & 65535) << 16;
-    wartosc = (wartosc & 4278255360) >> 8 | (wartosc & 16711935) << 8;
-    wartosc = (wartosc & 4042322160) >> 4 | (wartosc & 252645135) << 4; 
-    wartosc = (wartosc & 3435973836) >> 2 | (wartosc & 858993459) << 2;
-    wartosc = (wartosc & 2863311530) >> 1 | (wartosc & 1431655765) << 1;
-    
+    wartosc = (wartosc & 0b11111111111111110000000000000000) >> 16 | (wartosc & 0b00000000000000001111111111111111) << 16;
+    wartosc = (wartosc & 0b11111111000000001111111100000000) >> 8 | (wartosc & 0b00000000111111110000000011111111) << 8;
+    wartosc = (wartosc & 0b11110000111100001111000011110000) >> 4 | (wartosc & 0b00001111000011110000111100001111) << 4;
+    wartosc = (wartosc & 0b11001100110011001100110011001100) >> 2 | (wartosc & 0b00110011001100110011001100110011) << 2;
+    wartosc = (wartosc & 0b10101010101010101010101010101010) >> 1 | (wartosc & 0b01010101010101010101010101010101) << 1;
+
 	return wartosc;
 }
 
 
-int drugiSposob(int wartosc) 
+int drugiSposob(int wartosc)
 {
 	unsigned int buffor;
 	unsigned int potega;
 	unsigned int odwroconaWartosc = 0;
 	short przesuniecie = 7;
-	for (int  i = 32; i >= 16; i--) {
-		potega = pow(2, i);
-		buffor = wartosc;
-		
-		buffor = buffor & potega;
-		
-		buffor = buffor >> przesuniecie;
-		przesuniecie -= 2;
-		
-		odwroconaWartosc = odwroconaWartosc | buffor;
-	}
-	przesuniecie = 1;
-	for (int  i = 15; i >= 0; i--) {
-		potega = pow(2, i);
-		buffor = wartosc;
-		
-		buffor = buffor & potega;
-		
-		buffor = buffor << przesuniecie;
-		
-		przesuniecie += 2;
-		
-		odwroconaWartosc = odwroconaWartosc | buffor;
-	}
+	int i;
 	
+	for (i = 32; i >= 0; i--) {
+		potega = pow(2, i);
+		buffor = wartosc;
+		
+		buffor = buffor & potega;
+		
+		if (i <= 15) {
+			buffor = buffor << przesuniecie;
+			przesuniecie += 2;
+		} else {
+			buffor = buffor >> przesuniecie;
+			przesuniecie -= 2;
+		}
+		
+		if (przesuniecie >= 0) {
+			przesuniecie = 1;
+		}
+		
+		odwroconaWartosc = odwroconaWartosc | buffor;
+	}
 	return odwroconaWartosc;
 }
 
-int main(void) 
-{        
-	unsigned int wartosc = 2;
-	
-	printf("%d => %d \n", wartosc, pierwszySposob(wartosc));
-	printf("%d => %d \n", wartosc, drugiSposob(wartosc));
-	
-    
-    return 0;
+unsigned int trzeciSposob(unsigned int num)
+{
+    unsigned int count = sizeof(num) * 32 - 1;
+    unsigned int reverse_num = num;
+
+    num >>= 1;
+    while(num)
+    {
+       reverse_num <<= 1;
+       reverse_num |= num & 1;
+       num >>= 1;
+       count--;
+    }
+    reverse_num <<= count;
+    return reverse_num;
 }
 
+int main(void)
+{
+	unsigned int wartosc = 2;
+
+    printf("Liczba: %d\n", wartosc);
+
+    printf("%d\n", pierwszySposob(wartosc));
+    printf("%d\n", drugiSposob(wartosc));
+    printf("%d\n", trzeciSposob(wartosc));
+
+    return 0;
+}
